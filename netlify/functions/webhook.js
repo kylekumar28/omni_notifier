4940ee65364c5fae879ab39fa2de19701935c76a
Kyle Kumar <42920083+kylekumar28@users.noreply.github.com>
2024-11-30 18:27:02 +0000

    eset

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -2,12 +2,18 @@ const admin = require("firebase-admin");
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
+	// credential: admin.credential.cert(serviceAccount),
 	credential: admin.credential.applicationDefault(),
 	databaseURL:
 		"https://omnnotifier-default-rtdb.europe-west1.firebasedatabase.app/",
 });
 
 const db = admin.database();
+
+db.ref("messages")
+	.push({ content: "Test message", timestamp: Date.now() })
+	.then(() => console.log("Message saved successfully"))
+	.catch((error) => console.error("Error saving message:", error));
 
 exports.handler = async (event, context) => {
 	if (event.httpMethod !== "POST") {
@@ -18,18 +24,29 @@ exports.handler = async (event, context) => {
 	}
 
 	try {
-		const message = JSON.parse(event.body); // Discord webhook payload
+		// Parse the TradingView alert JSON payload
+		const { content } = JSON.parse(event.body);
+
+		if (!content) {
+			return {
+				statusCode: 400,
+				body: 'Invalid payload: Missing "content"',
+			};
+		}
+
+		// Save the content to Firebase
 		const messagesRef = db.ref("messages");
-		await messagesRef.push(message);
+		await messagesRef.push({ content, timestamp: Date.now() });
 
 		return {
 			statusCode: 200,
 			body: "Message received and saved",
 		};
 	} catch (error) {
+		console.error("Error processing webhook:", error);
 		return {
 			statusCode: 500,
-			body: `Error: ${error.message}`,
+			body: `Internal Server Error: ${error.message}`,
 		};
 	}
 };
